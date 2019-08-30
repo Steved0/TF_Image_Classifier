aceeed17d3277c3477a05d8dfc9cb63aeae84f15
Steved0 <30738653+Steved0@users.noreply.github.com>
2019-08-30 11:40:47 -0400

    TensorFlow JS - KNN Image Class
    TensorFlow JS - KNN Image Class with added No Action button

---
+++ index.js
@@ -0,0 +1,93 @@
+const classifier = knnClassifier.create();
+const webcamElement = document.getElementById('webcam');
+var no_Action = false;
+//const  naction =document.getElementById('class-d');
+
+let net;
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  await setupWebcam();
+
+
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  // When clicking a button, add an example for that class.
+  document.getElementById('class-a').addEventListener('click', () => addExample(0));
+  document.getElementById('class-b').addEventListener('click', () => addExample(1));
+  document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  document.getElementById('class-d').addEventListener('click', Respond_NoAction_Click);
+  document.getElementById('class-a').addEventListener('click', Respond_Action_Click);
+  document.getElementById('class-b').addEventListener('click', Respond_Action_Click);
+  document.getElementById('class-c').addEventListener('click', Respond_Action_Click);
+
+  while (true) {
+    if (no_Action) {
+        console.log('No Action Taken Loop');
+        document.getElementById('console').innerText = `
+          No Action Taken
+        `;
+    }
+
+    else if (classifier.getNumClasses() > 0) {
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(webcamElement, 'conv_preds');
+      // Get the most likely class and confidences from the classifier module.
+      const result = await classifier.predictClass(activation);
+
+      const classes = ['A', 'B', 'C'];
+
+      document.getElementById('console').innerText = `
+        prediction: ${classes[result.classIndex]}\n
+        probability: ${result.confidences[result.classIndex]}
+      `;
+    }
+
+    await tf.nextFrame();
+  }
+}
+
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
+
+function Respond_NoAction_Click() {
+  console.log('No Action Taken');
+  no_Action = true;
+}
+
+function Respond_Action_Click() {
+  console.log('Take Action');
+  no_Action =false;
+}
+
+app();
